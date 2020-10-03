cb1a5a1ce8ee1a64c3637610cb9fd83ca9859757
mazso47 <61661792+mazso47@users.noreply.github.com>
2020-10-03 21:04:39 +0200

    v1

--- JX48WK_gyak_4/JX48WK_gyak_4/Form1.cs
+++ JX48WK_gyak_4/JX48WK_gyak_4/Form1.cs
@@ -7,14 +7,150 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace JX48WK_gyak_4
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            CreateExcel();
+
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+            "Kód",
+            "Eladó",
+            "Oldal",
+            "Kerület",
+            "Lift",
+            "Szobák száma",
+            "Alapterület (m2)",
+            "Ár (mFt)",
+            "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (var l in Flats)
+            {
+                values[counter, 0] = l.Code;
+                values[counter, 1] = l.Vendor;
+                values[counter, 2] = l.Side;
+                values[counter, 3] = l.District;
+
+
+                if (l.Elevator == false)
+                {
+                    values[counter, 4] = "Nincs";
+                }
+                else
+                {
+                    values[counter, 4] = "Van";
+                }
+                
+
+                values[counter, 5] = l.NumberOfRooms;
+                values[counter, 6] = l.FloorArea;
+                values[counter, 7] = l.Price;
+                values[counter, 8] = "=" + GetCell(counter, 7) + "/" + GetCell(counter, 8);
+                counter++;
+            }
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+            
+            Excel.Range r = xlSheet.get_Range(GetCell(1, 1), GetCell(headers.Length, Flats.Count));
+            r.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range r2 = xlSheet.get_Range(GetCell(1, 1), GetCell(1, Flats.Count));
+            r2.Font.Bold = true;
+            r2.Interior.Color = Color.LightYellow;
+
+            Excel.Range r3 = xlSheet.get_Range(GetCell(1, headers.Length), GetCell(Flats.Count, headers.Length));
+            r3.Interior.Color = Color.LightGreen;
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }

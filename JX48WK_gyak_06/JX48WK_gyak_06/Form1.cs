27c10f38b7bf8722b4288be15afc26e8fe063b62
mazso47 <61661792+mazso47@users.noreply.github.com>
2020-10-15 19:31:22 +0200

    v1

--- JX48WK_gyak_06/JX48WK_gyak_06/Form1.cs
+++ JX48WK_gyak_06/JX48WK_gyak_06/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JX48WK_gyak_06.Entities;
+using JX48WK_gyak_06.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,127 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace JX48WK_gyak_06
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
+        
         public Form1()
         {
             InitializeComponent();
+            GetCurrencies();
+            RefreshData();
+            comboBox1.DataSource = Currencies;
+        }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+
+                var rate = new RateData();
+                Rates.Add(rate);
+
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+
+                chartRateData.DataSource = Rates;
+
+                var series = chartRateData.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRateData.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
+            }
+
+
+
+
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+            GetExchangeRates();
+            dataGridView1.DataSource = Rates;
+        }
+
+     
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void GetCurrencies()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetCurrenciesRequestBody()
+            {
+                
+            };
+
+
+            var response = mnbService.GetCurrencies();
+
+            
+            var result = response.GetCurrenciesResult;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }

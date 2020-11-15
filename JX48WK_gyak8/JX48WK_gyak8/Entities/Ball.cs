7d66f985116ecd68ed706f4f7500aa246aa03236
mazso47 <61661792+mazso47@users.noreply.github.com>
2020-11-15 18:17:34 +0100

    v2

--- JX48WK_gyak8/JX48WK_gyak8/Entities/Ball.cs
+++ JX48WK_gyak8/JX48WK_gyak8/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JX48WK_gyak8.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,29 +9,15 @@ using System.Windows.Forms;
 
 namespace JX48WK_gyak8.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            this.AutoSize = false;
-            this.Width = 50;
-            this.Height = 50;
-            Paint += Ball_Paint;
-        }
+        
 
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-        public void MoveBall()
-        {
-            Left += 1;
-        }
+       
     }
 }

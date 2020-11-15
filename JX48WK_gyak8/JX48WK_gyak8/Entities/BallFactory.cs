ddb38ca6ab98fbe12e8ba475a5982f4508233ce3
mazso47 <61661792+mazso47@users.noreply.github.com>
2020-11-15 18:37:01 +0100

    v4

--- JX48WK_gyak8/JX48WK_gyak8/Entities/BallFactory.cs
+++ JX48WK_gyak8/JX48WK_gyak8/Entities/BallFactory.cs
@@ -1,6 +1,7 @@
 ﻿using JX48WK_gyak8.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,10 @@ namespace JX48WK_gyak8.Entities
 {
     public class BallFactory : IToyFactory
     {
+        public Color BallColor { get; set; }
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Ball(BallColor);
         }
     }
 }
--- JX48WK_gyak8/JX48WK_gyak8/Form1.cs
+++ JX48WK_gyak8/JX48WK_gyak8/Form1.cs
@@ -15,12 +15,17 @@ namespace JX48WK_gyak8
     public partial class Form1 : Form
     {
         private List<Toy> _toys = new List<Toy>();
+        private Toy _nextToy;
 
         private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set
+            {
+                _factory = value;
+                DisplayNext();
+            }
         }
         public Form1()
         {
@@ -52,6 +57,40 @@ namespace JX48WK_gyak8
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
         }
     }
 }
